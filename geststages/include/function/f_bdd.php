3042bc84352b033dc1b0c9c8e26b08e434fd77d5
ruru2a <rutily2a@gmail.com>
2020-04-15 13:08:08 +0200

    premier commit

---
+++ geststages/include/function/f_bdd.php
@@ -0,0 +1,18 @@
+﻿<?php
+
+function connexionBDD()
+{
+	try
+	{
+            $bdd = new PDO('mysql:host=10.22.40.186;port=3306;dbname=gestages;charset=utf8', 'root', 'rootadmin', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+            return $bdd;
+	}
+	catch(Exception $e)
+	{
+		$pdo_error = $e->getMessage();
+                return false;
+	}
+    
+}
+
+?>
